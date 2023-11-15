db7c9c2be1683df7e5cfec606b272ca225753aa5
Vasyl Bihan <bihanvasyl@gmail.com>
2023-11-15 20:34:49 +0200

    Finished implementing of loading data from json and saving it to DB. Created test.json with different dir structure.

--- WebApplication-Hierarchical-Directory-System/Controllers/HomeController.cs
+++ WebApplication-Hierarchical-Directory-System/Controllers/HomeController.cs
@@ -23,8 +23,12 @@ namespace WebApplication_Hierarchical_Directory_System.Controllers
         public IActionResult Index()
         {
             var root = _db.Directories.FirstOrDefault(x => x.Parent == null);
-            loadDirectory(root);
 
+            if (root == null)
+            {
+                return View(new MyDirectory());
+            }
+            loadDirectoryProps(root);
 
             return View(root);
         }
@@ -35,109 +39,125 @@ namespace WebApplication_Hierarchical_Directory_System.Controllers
             {
                 return View("Index", new MyDirectory());
             }
-            //var subfolder = GetById(id);
+
             var subfolder = _db.Directories.FirstOrDefault(x => x.Id == id);
-            loadDirectory(subfolder);
+            if (subfolder == null)
+            {
+                return View(new MyDirectory());
+            }
+            loadDirectoryProps(subfolder);
+
             return View("Index", subfolder);
         }
         [HttpPost("/getParent/{id}")]
         public IActionResult GetParent(int id)
         {
             var current = _db.Directories.FirstOrDefault(x => x.Id == id);
-            loadDirectory(current);
+            if (current == null)
+            {
+                return View(new MyDirectory());
+            }
+            loadDirectoryProps(current);
+
             if (current.Parent == null)
             {
                 return View("Index", current);
             }
             var parent = current.Parent;
-            loadDirectory(parent);
+            loadDirectoryProps(parent);
+
             return View("Index", parent);
-        }
-
-        public void loadDirectory(MyDirectory directory)
-        {
-            // set Directory Childrens
-            var childrens = _db.Directories.Where(x => x.Parent.Id == directory.Id).OrderBy(x => x.Id).ToList();
-            directory.Childrens.ToList().AddRange(childrens);
-            directory.Childrens = directory.Childrens.OrderBy(x => x.Id).ToList();
-            // set Parent
-            directory.Parent = _db.Directories.FirstOrDefault(x => x.Id == directory.ParentId);
-        }
-        [HttpGet("/saveToFile")]
-        public void saveToFile()
-        {
-            var data = _db.Directories.ToList();
-            string json = JsonConvert.SerializeObject(data.ToArray(), Formatting.Indented, new JsonSerializerSettings
-            {
-                PreserveReferencesHandling = PreserveReferencesHandling.Objects
-            });
-
-            var filename = Guid.NewGuid().ToString() + ".json";
-
-            System.IO.File.WriteAllText(Path.Combine(_webHostEnvironment.WebRootPath, "backups/", filename), json);
-        }
-        [HttpGet("/saveToFile")]
-        public List<MyDirectory> readFile(string path)
-        {
-            return JsonConvert.DeserializeObject<List<MyDirectory>>(System.IO.File.ReadAllText(path));
-        }
-        [HttpGet("/loadFile")]
-        public IActionResult loadFile()
-        {
-            // TO DO
-            // 1-st export existing db to json and delete
-            // 2nd inserting into db data from json file
-            
-            var data = readFile(@"data.json");
-            var root = data.FirstOrDefault();
-
-            return View("Index", root);
         }
         [HttpPost("/loadDirectoryTree")]
         public IActionResult loadFromFile([FromForm] IFormFile? file)
         {
-            if(file != null)
+            if (file != null)
             {
                 var filename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                 using (var f = System.IO.File.OpenWrite(Path.Combine(_webHostEnvironment.WebRootPath, "uploads/", filename)))
                 {
                     file.CopyTo(f);
                 }
-                // CHANGE
 
                 var data = readFile(Path.Combine(_webHostEnvironment.WebRootPath, "uploads/", filename));
-                if(data != null)
+                if (data != null)
                 {
-                    // TO DO
-                    // 1-st export existing db to json and delete
-                    // 2nd inserting into db data from json file
+                    // export existing db data to json and delete data from db
                     saveToFile();
                     _db.Database.ExecuteSqlRaw("TRUNCATE TABLE [Directories]");
-                    foreach (var item in data)
+                    var orderedList = data.OrderBy(x => x.Id);
+                    foreach (var item in orderedList)
                     {
                         var model = new MyDirectory
                         {
                             Title = item.Title,
                             ParentId = item.ParentId
                         };
+                        // inserting into db data from json file
                         _db.Add(model);
-                        _db.SaveChanges();
+                        _db.SaveChanges(); // must be here because of foreign key
                     }
                     //_db.SaveChanges();
                     var root = _db.Directories.FirstOrDefault(x => x.Parent == null);
-                    loadDirectory(root);
-
+                    if (root == null)
+                    {
+                        return View("Index", new MyDirectory());
+                    }
+                    loadDirectoryProps(root);
 
                     return View("Index", root);
                 }
-                
 
-                //var root = data.FirstOrDefault();
-                //return View("Index", root);
                 return View("Index", new MyDirectory());
             }
-            // error loading/reading file
+
             return View("Index", new MyDirectory());
+        }
+        public void saveToFile()
+        {
+            var data = _db.Directories.AsNoTracking().ToList();
+            if (data.Count > 0)
+            {
+                string json = JsonConvert.SerializeObject(data.ToArray(), Formatting.Indented, new JsonSerializerSettings
+                {
+                    PreserveReferencesHandling = PreserveReferencesHandling.Objects
+                });
+
+                var filename = Guid.NewGuid().ToString() + ".json";
+
+                System.IO.File.WriteAllText(Path.Combine(_webHostEnvironment.WebRootPath, "backups/", filename), json);
+            }
+        }
+        public List<MyDirectory> readFile(string path)
+        {
+            var result = new List<MyDirectory>();
+            try
+            {
+                var tryResult = JsonConvert.DeserializeObject<List<MyDirectory>>(System.IO.File.ReadAllText(path));
+                if (tryResult != null)
+                {
+                    result = tryResult;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+
+        }
+        public void loadDirectoryProps(MyDirectory directory)
+        {
+            if (directory == null)
+            {
+                return;
+            }
+            // set Directory Childrens
+            var childrens = _db.Directories.Where(x => x.Parent.Id == directory.Id).ToList();
+            directory.Childrens.ToList().AddRange(childrens);
+            directory.Childrens = directory.Childrens.OrderBy(x => x.Id).ToList();
+            // set Directory Parent
+            directory.Parent = _db.Directories.FirstOrDefault(x => x.Id == directory.ParentId);
         }
     }
 }
--- WebApplication-Hierarchical-Directory-System/Program.cs
+++ WebApplication-Hierarchical-Directory-System/Program.cs
@@ -1,13 +1,15 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using WebApplication_Hierarchical_Directory_System.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DirectoryContext>(options =>
 {
-    //options.UseSqlServer("Data Source=FENRIR-PC\\SQLEXPRESS;Initial Catalog=DirectoryStructure;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False");
-    options.UseSqlServer("Data Source=WINSRVR2019;Initial Catalog=DirectoryStructure;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;Command Timeout=0");
+    options.UseSqlServer("Data Source=FENRIR-PC\\SQLEXPRESS;Initial Catalog=DirectoryStructure;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False");
+    //options.UseSqlServer("Data Source=WINSRVR2019;Initial Catalog=DirectoryStructure;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;Command Timeout=0");
 });
 
 
@@ -17,7 +19,6 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 app.MapControllers();
-
 
 app.UseStaticFiles();
 
